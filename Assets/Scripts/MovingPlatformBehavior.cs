ef67a22ecc266da1afb53cbd494c604d913ed432
abadosa <albert.badosa@scopely.com>
2021-04-25 20:47:23 +0200

    Fixed plataforms weird physics

--- Assets/Scripts/MovingPlatformBehavior.cs
+++ Assets/Scripts/MovingPlatformBehavior.cs
@@ -11,6 +11,12 @@ public class MovingPlatformBehavior : MonoBehaviour
     public bool towardsB;
     private Vector3 startPosition;
     private Vector3 endPosition;
+    protected Rigidbody2D rb2d;
+    
+    void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 
     void Start()
     {
@@ -18,7 +24,6 @@ public class MovingPlatformBehavior : MonoBehaviour
         startPosition = platformA.transform.position;
         endPosition = platformB.transform.position;
         StartCoroutine(Vector3LerpCoroutine(gameObject, endPosition, moveSpeed));
-
     }
 
     // Update is called once per frame
@@ -41,13 +46,16 @@ public class MovingPlatformBehavior : MonoBehaviour
     //GameObject moves towards Target and stops when gets to target.
     IEnumerator Vector3LerpCoroutine(GameObject obj, Vector3 target, float speed)
     {
-        Vector3 startPosition = obj.transform.position;
+        Vector3 startPosition = rb2d.position;
         float time = 0f;
 
         while (obj.transform.position != target)
         {
-            obj.transform.position = Vector3.Lerp(startPosition, target, (time / Vector3.Distance(startPosition, target)) * speed);
-            // this.GetComponent<Rigidbody2D>().MovePosition(Vector2.Lerp(startPosition, target, (time / Vector2.Distance(startPosition, target)) * speed));
+            if (rb2d)
+            {
+                rb2d.position = Vector3.Lerp(startPosition, target,
+                    (time / Vector3.Distance(startPosition, target)) * speed);
+            }
             time += Time.deltaTime;
             yield return null;
         }
@@ -61,7 +69,6 @@ public class MovingPlatformBehavior : MonoBehaviour
         //col.gameObject.transform.parent = gameObject.transform;
         //col.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
     }
-
 
     void OnCollisionExit2D(Collision2D col)
     {
