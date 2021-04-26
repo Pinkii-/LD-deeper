3884e9edef2c4f739f5d2dada82674672c69894b
abadosa <albert.badosa@scopely.com>
2021-04-26 23:10:30 +0200

    Final scene tweaks

--- Assets/Scripts/LevelsController.cs
+++ Assets/Scripts/LevelsController.cs
@@ -3,11 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UIElements;
 using Object = System.Object;
 
 public class LevelsController : MonoBehaviour
 {
     public List<Transform> Levels;
+    public GameObject Player;
     public Transform finalLevel;
     public int LevelsGoal = 0;
     
@@ -100,9 +103,6 @@ public class LevelsController : MonoBehaviour
 
     private void TriggerEndgame()
     {
-        Debug.Log("YOU WIN MADAFACA!");
-        // Para hacer restart debería ser suficiente con llamar la función Init(). 
-        // ...
         float levelY = 0f;
 
         if (transform.childCount == 2)
@@ -125,9 +125,36 @@ public class LevelsController : MonoBehaviour
             gameObject.transform);
 
         level.name = "Level_" + NextLevelUnclamped;
+        
+        // Disable level elements and platforms (VERY BAD DONE)
+        transform.GetChild(0).Find("Elements").gameObject.SetActive(false);
+        transform.GetChild(0).Find("Platforms").gameObject.SetActive(false);
+        transform.GetChild(0).Find("Checkpoints").gameObject.SetActive(false);
+        
+        GameObject.Find("Slider").SetActive(false);
+        
+        Camera.main.GetComponent<CameraController>().ZoomTo(1f);
+
+        StartCoroutine(GrowingLight());
 
         NextLevel = (NextLevel + 1) % NumLevels;
         NextLevelUnclamped++;
+    }
+
+    private IEnumerator GrowingLight()
+    {
+        float initialGlowRadius = Player.GetComponentInChildren<Light2D>().pointLightOuterRadius;
+        float time = 0f;
+        
+        yield return new WaitForSeconds(4f);
+        
+        while (Player.GetComponentInChildren<Light2D>().pointLightOuterRadius < 10f)
+        {
+            Player.GetComponentInChildren<Light2D>().pointLightOuterRadius = Mathf.Lerp(initialGlowRadius, 20f, time / 3f);
+            time += Time.deltaTime;
+            
+            yield return new WaitForSeconds(0);
+        }
     }
 
     public int NumLevels => Levels.Count;
